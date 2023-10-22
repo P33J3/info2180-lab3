3755de16fdbf77826a199b5615af2cd193c77572
P33J3 <peterjonlawrence@gmail.com>
2023-10-22 12:06:38 -0500

    completed exercise 3

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,31 +1,53 @@
 
+
+
 window.onload = () => {
+
     let divClasses = document.querySelectorAll("#board > div");
+    divClasses.forEach((div) => {
+        div.classList.add("square")
+    });
 
-    divClasses.forEach((div) => {div.classList.add("square")});
+    let evenOdd = 1;
 
-    evenOdd = 1;
+    divClasses.forEach((div) => {
+        div.addEventListener("click", () => {
+            if (evenOdd % 2 === 1) {
 
-    divClasses.forEach((div) => {div.addEventListener("click", () => {
-        if(evenOdd % 2 === 1) {
+                div.innerHTML = 'X';
+                evenOdd++;
 
-            div.innerHTML = 'X';
-            evenOdd++;
+            } else {
+                div.innerHTML = 'O';
+                evenOdd++;
+            }
 
-        } else {
-            div.innerHTML = 'O';
-            evenOdd++;
+            if (div.innerHTML === 'X') {
+                div.classList.remove('O');
+                div.classList.add('X');
+            } else {
+                div.classList.remove('X');
+                div.classList.add('O')
+            }
+
+        })
+    })
+
+    divClasses.forEach(div => {
+
+        div.onmouseover = () => {
+            div.classList.add("hover")
         }
 
-        if(div.innerHTML === 'X') {
-            div.classList.remove('O');
-            div.classList.add('X');
-        } else {
-            div.classList.remove('X');
-            div.classList.add('O')
+        div.onmouseleave = () => {
+            div.classList.remove("hover");
         }
-
-    })})
-
-
+    })
 }
+
+
+
+
+
+
+
